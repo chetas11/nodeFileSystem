cb68716d89277156b5456ae8a353f525d24305bb
chetas11 <cbirmole@gmail.com>
2020-11-11 05:39:03 +0530

    FileCreation

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ let current_datetime = new Date()
 let FileName = current_datetime.getFullYear() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() + "_" + current_datetime.getHours() + "-" + current_datetime.getMinutes() + "-" + current_datetime.getSeconds() 
 //to store current date & time.
 
-
-console.log(FileName)
     app
     .get("/", (req,res) =>{                                     
             fs.appendFile(FileName+".txt", FileContent,()=>{       //create a new file and appends the time-stamp
-                console.log("Updated")
+                console.log("File Created!")
                 res.send(`<h1>File Created</h1>`)
             })
-    }).listen(5000)
+    }).listen(process.env.PORT) 
